1fd3d7dda9e600aa8dd395a254ced5d76b04737f
marcelinho21junior <msbcontjr@outlook.com>
2021-12-28 11:17:42 -0300

    commit atualizado minutos 21

--- Page/script.js
+++ Page/script.js
@@ -4,7 +4,8 @@ function carregar(){
 
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML =`Agora são ${hora} horas`
+    var minuto = data.getMinutes()
+    msg.innerHTML =`Agora são ${hora}:${minuto} horas`
 
     if(hora >= 0 && hora < 12){
         //BOM DIA
